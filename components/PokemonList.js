d8106ece4b3fe0409766e3e2050ed4722c9cf764
Simone <simonesnehvide@gmail.com>
2026-09-25 12:36:05 +0200

    Tilføjede infinate scroll

--- components/PokemonList.js
+++ components/PokemonList.js
@@ -1,32 +1,25 @@
-export default function PokemonList() {
+export default function PokemonList(pokemon) {
 
     let pokemonListElement = document.createElement("section");
     pokemonListElement.classList.add("pokemon-list");
 
-    fetch("https://pokeapi.co/api/v2/pokemon?limit=60")
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (data) {
+    pokemon.forEach(function (pokemon) {
 
-            data.results.forEach(function (pokemon) {
-
-                const pokeUrl = pokemon.url.split("/");
-                const id = pokeUrl[6];
+        const pokeUrl = pokemon.url.split("/");
+        const id = pokeUrl[6];
 
 
-                pokemonListElement.innerHTML += `
+        pokemonListElement.innerHTML += `
                     <div class="pokemon">
                         <a href="detail.html?name=${pokemon.name}">
-                            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png" 
+                            <img loading="lazy" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png" 
                             alt="${pokemon.name}" class="pokemon-image">
                               <p class="pokemon-number">#${id.padStart(3, "0")}</p>
                             <p class="pokemon-name">${pokemon.name}</p>
                         </a>
                     </div>
                 `
-            });
-        });
+    });
 
     return pokemonListElement;
 }
--- js/script.js
+++ js/script.js
@@ -1,6 +1,30 @@
 import PokemonList from "../components/PokemonList.js";
 import Header from "../components/Header.js";
 
+let pokemon = [];
+let customOffset = 0;
+
+let observer = new IntersectionObserver(function (entries) {
+    entries.forEach(function (entry) {
+        if (entry.isIntersecting) {
+
+            observer.unobserve(entry.target);
+
+            customOffset = customOffset + 60;
+
+            fetch(`https://pokeapi.co/api/v2/pokemon?offset=${customOffset}&limit=60`)
+                .then(function (response) {
+                    return response.json();
+                })
+                .then(function (data) {
+                    pokemon = [...pokemon, ...data.results]
+                    render()
+                });
+
+        }
+    });
+});
+
 let rootElement = document.querySelector("#root");
 
 function render() {
@@ -9,13 +33,24 @@ function render() {
 
     let mainElement = document.createElement("main");
 
-    mainElement.append(PokemonList());
+    mainElement.append(PokemonList(pokemon));
 
     rootElement.append(mainElement);
+
+    let fiftheLastElement = document.querySelector(".pokemon:nth-last-of-type(5)");
+    observer.observe(fiftheLastElement);
 }
 
 function init() {
-    render();
+    fetch("https://pokeapi.co/api/v2/pokemon?offset=0&limit=60")
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            pokemon = [...data.results];
+
+            render();
+        });
 }
 
 init()
